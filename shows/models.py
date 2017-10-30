58de953a583d8c9458d7d686e58e6c29ec842a46
Marcus Crane <marcus@thingsima.de>
2017-10-30 17:13:25 +1300

    It's alive! Able to successfully fetch show information in the background

--- shows/models.py
+++ shows/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Show(models.Model):
+  tmdb_id = models.IntegerField('TheMovieDB ID Number')
+  name = models.CharField('Show title', max_length=200)
+  slug = models.SlugField('URL shortcut', max_length=40)
+  language = models.CharField('Language', max_length=50, blank=True)
+  episodes = models.IntegerField('Number of episodes', blank=True)
+  seasons = models.IntegerField('Number of seasons', blank=True)
+  overview = models.TextField('General overview', blank=True)
+  imported = models.DateField('First imported', blank=True, null=True)
--- shows/tasks.py
+++ shows/tasks.py
@@ -1,24 +1,28 @@
 from django.conf import settings
+from django.utils import timezone
+from django.utils.text import slugify
 from celery import shared_task
 
 from .models import Show
 
 import requests
 
-logger = get_task_logger(__name__)
-
 @shared_task
-def import_show(tmdb_id):
+def fetch_show(tmdb_id):
     """ Imports show details from TheMovieDB """
-    r = requests.get('https://api.themoviedb.org/3/tv/{}?api_key={}'.format(tmdb_id, settings.TMDB_KEY))
+    url = 'https://api.themoviedb.org/3/tv/{}?api_key={}'.format(tmdb_id, settings.TMDB_KEY)
+    r = requests.get(url)
     show = r.json()
 
     name = show['name']
+    slug = slugify(name)
     episodes = show['number_of_episodes']
     seasons = show['number_of_seasons']
-    languages = show['languages']
+    language = show['original_language']
     overview = show['overview']
+    imported = timezone.now()
 
-    Show.objects.create(tmdb_id=tmdb_id, name=name, episodes=episodes, seasons=seasons, language=language, overview=overview)
+    Show.objects.create(tmdb_id=tmdb_id, name=name, slug=slug, episodes=episodes,
+            seasons=seasons, language=language, overview=overview, imported=imported)
 
-    return "Successfully imported {} from TMDB".format(show.name)
+    return "Successfully imported {} from TMDB".format(name)
