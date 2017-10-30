e2d74b9e3d78889646724aedaf343a9aa3cdb3d5
Marcus Crane <marcus@thingsima.de>
2017-10-30 22:42:41 +1300

    Test rendering seasons + episodes

--- shows/views.py
+++ shows/views.py
@@ -1,6 +1,7 @@
+from django.shortcuts import get_object_or_404, render
 from django.views import generic
 
-from .models import Show
+from .models import Show, Season, Episode
 
 class IndexView(generic.ListView):
   template_name = 'shows/index.html'
@@ -8,3 +9,10 @@ class IndexView(generic.ListView):
 
   def get_queryset(self):
     return Show.objects.all()
+
+def seasons(request, slug):
+  show = get_object_or_404(Show, slug=slug)
+  seasons = Season.objects.filter(show=show)
+  episodes = Episode.objects.filter(season=seasons[0])
+  context = {'seasons': seasons, 'episodes': episodes}
+  return render(request, 'shows/seasons.html', context)
